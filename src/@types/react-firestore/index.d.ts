379a6da143f458e9b743efc7986b19a54450f729
Konrad Klimczak <konradklimczak93@gmail.com>
2019-11-08 22:55:53 +0100

    Base for app

---
+++ src/@types/react-firestore/index.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-firestore' {
+  import React from 'react';
+  export class FirestoreProvider extends React.Component<any> {}
+
+  export interface IFirestoreCollectionProps {
+    path: string;
+    render(args: any): React.ReactNode;
+  }
+  export class FirestoreCollection extends React.Component<IFirestoreCollectionProps> {}
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from './components/AppBar/AppBar';
+import ScoreTable from './components/ScoreTable/ScoreTable';
+import { makeStyles } from '@material-ui/core';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <AppBar />
+      <ScoreTable />
     </div>
   );
-}
+};
 
 export default App;
